aa71ebbf257004b4a525c4e44e40287ab7a5282c
Gloria Geraldine Kalvadet <geraldinekalvadet27@gmail.com>
2023-01-18 11:45:01 +0200

    ooo

--- js/script.js
+++ js/script.js
@@ -20,3 +20,46 @@ if(lengthOfUsername >= 4 && lengthOfUsername <=10){
     console.log ("Too many characters");
   }
 }
+
+//* Question 3 *//
+// The invoice must be paid
+// The product must have been dispatched
+// The customer must have signed for the delivery
+
+var invoicePaid = true;
+var productDispatched = false;
+var customerHasSigned = false;
+
+if ((invoicePaid === true) && (productDispatched === true) && (customerHasSigned === true)){
+  console.log ("The order is completed");
+} else {
+  if (productDispatched === false){
+    console.log ("The product is not dispatched");
+  }
+  if (customerHasSigned === false){
+    console.log ("The customer has not signed");
+  }
+}
+
+//* Question 4 *//
+// The invoice must be paid
+// The product must have been dispatched
+// The customer must have signed for the delivery
+
+var invoicePaid = true;
+var productDispatched = true;
+var customerHasSigned = true;
+
+if ( !invoicePaid || !productDispatched || !customerHasSigned){
+  if (!invoicePaid){
+    console.log ("The invoice is not paid");
+  }
+  if (!productDispatched){
+    console.log ("The product is not dispatched");
+  }
+  if (!customerHasSigned){
+    console.log ("The customer has not signed");
+  }
+} else {
+  console.log ("Order completed")
+}
